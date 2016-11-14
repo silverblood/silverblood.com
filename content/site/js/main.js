965aa8512d3402c64ea5dd9b8b445dcde038185f
Jonathon Rossi <jono@jonorossi.com>
2016-11-15 00:52:42 +1000

    Rework web site using Feedrabbit's contact page

---
+++ content/site/js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+  // bootstrap tooltips
+  $("[data-toggle=tooltip]").tooltip({
+    'container': 'body'
+  });
+
+  // populate the electronic letter delivery hyperlinks to hide them from the source
+  $("a.electronic-letter-delivery").each(function () {
+    var x = new Array("silverblood", ".c", "om").join("");
+    var y = new Array("admin", x).join("@");
+    var z = new Array("ma", "il", "to:", y).join("");
+    $(this).text(y);
+    $(this).attr("href", z);
+  });
+});
